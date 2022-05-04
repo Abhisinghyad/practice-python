d3f0e953f7b28a732fd5eb77b9a209eda7a9f5e2
Abhisinghyad <92367779+Abhisinghyad@users.noreply.github.com>
2022-05-04 23:38:10 +0530

    Add files via upload

---
+++ Write a Python Program to swap two numbers using the third variable..py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the First number: "))
+num2 = int(input("Enter the Second Number: "))
+a=num2
+b=num1
+
+print("Swapping of First Number: ", a)
+print("Swapping of Second Number: ",b)
