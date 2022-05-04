3ce53a760e20cbbd4a601756aff906d89cf36f6a
Abhisinghyad <92367779+Abhisinghyad@users.noreply.github.com>
2022-05-04 23:41:04 +0530

    Swap Number using third variable and without third

---
+++ Write a PythonProgram to swap two numbers without using the third variable..py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the First number: "))
+num2 = int(input("Enter the Second Number: "))
+swap = num1
+a = num2
+num2 = swap
+
+print("Swapping of First Number: ", a)
+print("Swapping of Second Number: ", num2)
